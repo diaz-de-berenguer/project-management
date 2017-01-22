d337864a28e01f6d8bce28bd2ccbbd764a1ae33b
Diego Diaz de Berenguer <d.diazdeberenguer@gmail.com>
2017-01-22 01:16:16 -0600

    Add scaffold controller for projects

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   resources :teams, only: [:new, :create, :destroy]
   post "/teams/switch/:id" => 'teams#switch', as: 'team_switch'
 
+  resources :projects
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
