53f585a031ba1f9738703625b2eba8d047c77c84
Diego Diaz de Berenguer <d.diazdeberenguer@gmail.com>
2017-01-23 17:52:36 -0600

    Remove active product Id from memberships after destroy

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,17 @@ class Product < ActiveRecord::Base
   belongs_to :project
   validates_presence_of :name, :project_id
 
+  before_destroy :set_active_products_to_nil
+
   def self.sorted
   	all.sort_by{ |p| p.name.downcase }
   end
+
+  private
+
+	  def set_active_products_to_nil
+	  	# loop through the memberships that have this product ID as 'active_product' and
+	  	# set value to nil.
+	  	TeamMembership.where(active_product_id: self.id).map { |m| m.update(active_product_id: nil) }
+	  end
 end
